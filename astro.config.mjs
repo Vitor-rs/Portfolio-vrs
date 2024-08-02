c3659d7201f068f18036c05bbc7916931547ec8d
Vitor Santos <vitor.santos9@estudante.ifms.edu.br>
2024-08-02 18:04:39 -0400

    feat: Update base URL in Astro configuration file
    The base URL in the Astro configuration file was updated to '/portfolio-vrs' to ensure proper routing for the portfolio website.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [react(), tailwind()],
   site: 'https://vitor-rs.github.io',
-  base: 'portfolio-vrs'
+  base: '/portfolio-vrs'
 });
