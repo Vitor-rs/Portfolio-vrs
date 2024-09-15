ed88d9474eff2a6991b0c0bfb36eb4f31fff1038
Vitor-rs <vitor.santos9@estudante.ifms.edu.br>
2024-09-15 17:15:39 -0400

    Consertando rotas

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+    base: '/portfolio-vrs/', // Substitua <nome-do-repositorio> pelo nome do seu repositório
+    integrations: [tailwind()]
 });
