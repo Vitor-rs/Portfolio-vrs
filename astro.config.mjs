f2aa61432eb5dc3fa3d157d6e2f21e020858e950
Vitor-rs <vitor.santos9@estudante.ifms.edu.br>
2024-09-15 21:07:17 -0400

    Customizações e ajustes granulares

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-    base: '/portfolio-vrs/', // Substitua <nome-do-repositorio> pelo nome do seu repositório
+    base: '/portfolio-vrs/',
     integrations: [tailwind()]
 });
